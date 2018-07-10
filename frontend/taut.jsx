3fac2140fd5b20d64b90a852b313b1a842dbec5a
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-10 17:08:36 -0400

    Heroku ready commit

--- frontend/taut.jsx
+++ frontend/taut.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<Root store={store}/>, rootEl);
 
   // TODO: Delete before deployment
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 });
+
+// TODO: Delete before deployment
+import * as SessionApiUtil from './utils/session_api_util';
+window.SessionApiUtil = SessionApiUtil;
