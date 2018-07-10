b966980de6041b286846a1660af8c99b4712c69e
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-10 15:11:18 -0400

    Added session error actions and reducers.

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionApiUtil from '../utils/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const receiveCurrentUser = (user) => dispatch => {
   return {
@@ -17,20 +17,30 @@ export const logoutCurrentUser = () => dispatch => {
   };
 };
 
+export const receiveErrors = errors  => dispatch => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors,
+  };
+};
+
 export const login = user => dispatch => {
   SessionApiUtil.login(user).then(
-    user => dispatch(receiveCurrentUser(user))
+    user => dispatch(receiveCurrentUser(user)) ,
+    errors => dispatch(receiveErrors(errors))
   );
 };
 
 export const logout = () => dispatch => {
   SessionApiUtil.logout().then(
-    () => dispatch(logoutCurrentUser())
+    () => dispatch(logoutCurrentUser()) ,
+    errors => dispatch(receiveErrors(errors))
   );
 };
 
 export const signup = user => dispatch => {
   SessionApiUtil.signup(user).then(
-
+    user => dispatch(receiveCurrentUser(user)) ,
+    errors => dispatch(receiveErrors(errors))
   );
 };
---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+import session from './errors/session_errors_reducer';
+
+export default combineReducers({session});
