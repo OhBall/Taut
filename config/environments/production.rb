7963b813b78d2dcfbd1b325aadf58cbc19bc0456
Oliver Ball <olivermartinball@gmail.com>
2018-07-16 10:47:51 -0400

    comment out action_cable lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,10 @@ Rails.application.configure do
 
   # TODO: comment out the comment below
   # Will need to add frontend domain names to config.actio_tables.allowed_request_origins
-  config.action_cable.mount_path = '/cable'
+  # config.action_cable.mount_path = '/cable'
   # config.action_cable.url = 'wss://localhost:3000/cable'
   # config.action_cable.allowed_request_origins = [ /http:\/\/localhost:*/ ]
-  config.web_socket_server_url = "wss://slack-to-taut.herokuapp.com/cable"
-  config.action_cable.url = "wss://http//slack-to-taut.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://slack-to-taut.herokuapp.com', 'http://slack-to-taut.herokuapp.com', /http:\/\/slack-to-taut.herokuapp.*/]
+  # config.web_socket_server_url = "wss://slack-to-taut.herokuapp.com/cable"
+  # config.action_cable.url = "wss://http//slack-to-taut.herokuapp.com/cable"
+  # config.action_cable.allowed_request_origins = ['https://slack-to-taut.herokuapp.com', 'http://slack-to-taut.herokuapp.com', /http:\/\/slack-to-taut.herokuapp.*/]
 end
