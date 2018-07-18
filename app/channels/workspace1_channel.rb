6f528968a2da47c807f3c54952b86726547e15ed
Oliver Ball <olivermartinball@gmail.com>
2018-07-18 13:41:02 -0400

    action cables now only allow subscriptions with permission

--- app/channels/workspace1_channel.rb
+++ app/channels/workspace1_channel.rb
@@ -1,7 +1,8 @@
 class Workspace1Channel < ApplicationCable::Channel
   def subscribed
     channel_room = Channel.find(@params['id'])
-    stream_for channel_room
+    # stream_for channel_room
+    stream_for channel_room if (!channel_room.private || channel_room.users.include?(current_user))
   end
 
   def unsubscribed
