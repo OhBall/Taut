8f15da6bacce20b3c4fefb5d7ecf2ad6196f0090
Oliver Ball <olivermartinball@gmail.com>
2018-07-18 15:22:51 -0400

    Main now a React Component that requests permissions on mount

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import ChannelSidebar from '../channel-sidebar/channel_sidebar_container';
 import Messenger from '../messenger/messenger';
 import ModalConductor from '../modals/modal_conductor';
 
-const Main = props => {
-  return (
-    <div className='main'>
-      <ModalConductor />
-      <ChannelSidebar />
-      <Messenger />
-    </div>
-  );
+import { requestPermissions } from '../../actions/permission_actions';
+
+const mapDispatchToProps = dispatch => {
+  return {
+    requestPermissions: () => dispatch(requestPermissions())
+  };
 };
 
-export default Main;
+class Main extends React.Component {
+
+  componentDidMount(){
+    this.props.requestPermissions();
+  }
+
+  render(){
+    return (
+      <div className='main'>
+        <ModalConductor />
+        <ChannelSidebar />
+        <Messenger />
+      </div>
+    );
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Main);
