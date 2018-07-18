49a8cebe07823aeaf0aa32fe2268adedf33a5b9d
Oliver Ball <olivermartinball@gmail.com>
2018-07-18 14:15:41 -0400

    remove debugger

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -9,15 +9,16 @@ class Api::ChannelsController < ApplicationController
       if @channel.private
         current_user_id = current_user.id
         permission_params.each do |user_id|
-          debugger
           unless user_id == current_user_id
             @channel.permissions.build(user_id: user_id)
           end
         end
         @channel.permissions.build(user_id: current_user_id)
       end
-      @channel.save
-      render :show
+      if @channel.save
+        render :show
+      else
+        render :show, status 422
     else
       render json: @channel.errors.full_messages
     end
