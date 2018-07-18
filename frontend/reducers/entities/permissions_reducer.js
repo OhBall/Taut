3b3232189cf3dcfbd9183cdb1e65db5103be4e0d
Oliver Ball <olivermartinball@gmail.com>
2018-07-18 16:52:32 -0400

    Users gain permission to channel on channel creation

--- frontend/reducers/entities/permissions_reducer.js
+++ frontend/reducers/entities/permissions_reducer.js
@@ -1,6 +1,7 @@
 import { merge } from 'lodash';
 
 import { RECEIVE_PERMISSIONS } from '../../actions/permission_actions';
+import { RECEIVE_CHANNEL } from '../../actions/channel_actions';
 
 const PermissionsReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -8,6 +9,10 @@ const PermissionsReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_PERMISSIONS:
       return action.permissions;
+    case RECEIVE_CHANNEL:
+      const channelId = action.channel.id;
+      merge(copyState, {[channelId]: {channelId}});
+      return copyState;
     default:
       return state;
   }
