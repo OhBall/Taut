8749ae29ae7a2a74e86bcfd3bdf129f80521d323
Oliver Ball <olivermartinball@gmail.com>
2018-07-19 14:59:20 -0400

    remove whitespace

--- frontend/components/modals/dm_form_modal.jsx
+++ frontend/components/modals/dm_form_modal.jsx
@@ -5,7 +5,6 @@ import SearchableUserList from '../forms/searchable_user_list_container';
 class DmFormModal extends React.Component {
 
   render() {
-
     return (
       <form className='dm-form modal-form'>
         <h1>Direct Messages</h1>
