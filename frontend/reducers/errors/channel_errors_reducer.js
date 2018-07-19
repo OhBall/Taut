b25e9586c98e6727d12f464ae21b89640644843e
Oliver Ball <olivermartinball@gmail.com>
2018-07-19 14:00:57 -0400

    Implement clearing of channel errors

--- frontend/reducers/errors/channel_errors_reducer.js
+++ frontend/reducers/errors/channel_errors_reducer.js
@@ -1,10 +1,14 @@
 import { RECEIVE_CHANNEL,
          RECEIVE_CHANNEL_ERRORS, } from '../../actions/channel_actions';
+import { CLEAR_MODAL } from '../../actions/modal_actions';
 
 const SessionErrorsReducer = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_CHANNEL_ERRORS:
       return action.errors;
+    case CLEAR_MODAL:
+    case RECEIVE_CHANNEL:
+      return [];
     default:
       return state;
   }
