ac9a77ab1ea841c78dea4aeee424283cc2e88ffc
Oliver Ball <olivermartinball@gmail.com>
2018-07-19 09:57:12 -0400

    convert MessageListItem to a React component

--- frontend/components/messenger/message_list_item.jsx
+++ frontend/components/messenger/message_list_item.jsx
@@ -1,42 +1,57 @@
 import React from 'react';
 
-const MessageListItem = ({message, user}) => {
+// const MessageListItem = ({message, user}) => {
+class MessageListItem extends React.Component {
 
-  const { body, created_at } = message;
+  constructor(props){
+    super(props);
 
-  let username = '';
-  let img_url = '';
-  if (user) {
-    username = user.username;
-    img_url = user.img_url;
-  }
+    const date = new Date(this.props.message.created_at);
 
-  const date = new Date(created_at);
+    let hours = date.getHours();
+    const ampm = hours < 12 ? 'AM' : 'PM';
+    hours = hours % 12;
+    if (hours === 0) hours = 12;
 
-  let hours = date.getHours();
-  const ampm = hours < 12 ? 'AM' : 'PM';
-  hours = hours % 12;
-  if (hours === 0) hours = 12;
+    let minutes = date.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
 
-  let minutes = date.getMinutes();
-  if (minutes < 10) minutes = '0' + minutes;
-
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Super-secret-special-day'];
-  const time = days[date.getDay()] + ', ' +
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Super-secret-special-day'];
+    this.time = days[date.getDay()] + ', ' +
                 hours + ':' + minutes + ampm;
 
-  return(
-    <li>
-      <div className='profile-picture'/>
-      <section className='words'>
-        <header className='message-info'>
-          <div className='username'>{username}</div>
-          <div className='timestamp'>{time}</div>
-        </header>
-        <p>{body}</p>
-      </section>
-    </li>
-  );
+  }
+
+
+
+
+  render() {
+    const { body, created_at } = this.props.message;
+
+    let username = '';
+    let img_url = '';
+    if (this.props.user) {
+      username = this.props.user.username;
+      img_url = this.props.user.img_url;
+    }
+
+    return(
+      <li >
+        <div className='profile-picture'/>
+        <section className='words'>
+          <header className='message-header'>
+            <div className ='message-info'>
+              <span className='username'>{username}</span>
+              <span className='timestamp'>{this.time}</span>
+            </div>
+            <button className='delete-message-button'>&#10005;</button>
+          </header>
+          <p>{body}</p>
+        </section>
+      </li>
+    );
+  }
+
 
 };
 
