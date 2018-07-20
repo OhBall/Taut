e4481a4dcbec1f46a8c6f5a9d0418676a765ec78
Oliver Ball <olivermartinball@gmail.com>
2018-07-20 10:32:40 -0400

    Cleanup development code

--- frontend/taut.jsx
+++ frontend/taut.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
 import actionCable from 'actioncable';
 
 import Root from './components/root';
-
-
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-
   let preloadedState = {};
   const { currentUser } = window;
   if (currentUser){
@@ -21,18 +18,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore(preloadedState);
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, rootEl);
-
-
-
-  // TODO: Delete before deployment
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
 });
-
-// TODO: Delete before deployment
-
-import * as ChannelApiUtil from './utils/channel_api_util';
-window.cau = ChannelApiUtil;
-
-import * as ModalActions from './actions/modal_actions';
-window.ModalActions = ModalActions;
