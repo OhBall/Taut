84b4ac04cc56659b67d37dba7ad6a483b51a4e76
Oliver Ball <olivermartinball@gmail.com>
2018-07-26 15:40:43 -0400

    Convert main nav to React Component. Add delete channel button.

--- frontend/components/messenger/navbar/main_navbar.jsx
+++ frontend/components/messenger/navbar/main_navbar.jsx
@@ -1,31 +1,47 @@
 import React from 'react';
 
-const MainNavbar = ({selectedConvo, users, currentUserId}) => {
-  let h1 = '';
-  let h2 = '';
-  if (selectedConvo.is_dm) {
-    const usernames = [];
-    selectedConvo.user_ids.forEach( userId => {
-      if (userId !== currentUserId){
-        usernames.push(users[userId].username);
-      }
-    });
-    h1 = usernames.join(', ');
-  } else {
-    h1 = `# ${selectedConvo.name}`
-    h2 = selectedConvo.description
+class MainNavbar extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
-  return (
-    <header className='main-navbar'>
-      <div className='channel-info'>
-        <h1>{h1}</h1>
-        <h2>{h2}</h2>
-      </div>
-      <div className='options'>
-      </div>
-    </header>
-  );
-};
+  handleDelete(){
+    this.props.deleteChannel(this.props.selectedConvo.id);
+  }
+
+  render(){
+    const {selectedConvo, users, currentUserId} = this.props;
+
+    let h1 = '';
+    let h2 = '';
+    if (selectedConvo.is_dm) {
+      const usernames = [];
+      selectedConvo.user_ids.forEach( userId => {
+        if (userId !== currentUserId){
+          usernames.push(users[userId].username);
+        }
+      });
+      h1 = usernames.join(', ');
+    } else {
+      h1 = `# ${selectedConvo.name}`;
+      h2 = selectedConvo.description;
+    }
+
+    return (
+      <header className='main-navbar'>
+        <div className='channel-info'>
+          <h1>{h1}</h1>
+          <h2>{h2}</h2>
+        </div>
+        <div className='options'>
+          <button onClick={this.handleDelete}>Delete Channel</button>
+        </div>
+      </header>
+    );
+  }
+
+}
 
 export default MainNavbar;
